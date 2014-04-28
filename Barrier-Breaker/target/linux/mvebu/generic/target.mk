c68d2904e3fb1cb6f589e87553b3819d79277017
Matthew Fatheree <Matthew.Fatheree@belkin.com>
2014-04-28 13:45:24 -0700

    removing source files will replace with patches

--- Barrier-Breaker/target/linux/mvebu/generic/target.mk
+++
@@ -1,5 +0,0 @@
-BOARDNAME:=Generic
-
-define Target/Description
-	Build firmware images for generic Marvell Armada XP/370 
-endef
--- Barrier-Breaker/target/linux/x86/generic/target.mk
+++
@@ -1,9 +0,0 @@
-BOARDNAME:=Generic
-
-LINUX_VERSION:=3.10.32
-
-define Target/Description
-	Build firmware images for x86 based boards
-	(e.g : Soekris, ...)
-endef
-
